f61d932b990dc2c1fe182e6d1ba91cd6b3476e83
Yifan Guang <yguang@sandshark.ics.cs.cmu.edu>
2023-09-18 15:08:52 -0400

    update CMakeLists and ifndef to logger.h

--- mixnet/logger.h
+++ mixnet/logger.h
@@ -1,3 +1,5 @@
+#ifndef LOGGER_H
+#define LOGGER_H
 #include "address.h"
 
 #include <stdarg.h>
@@ -11,3 +13,5 @@ void logger_init(bool turn_on, mixnet_address node_addr);
 void print(const char *format, ...);
 void print_err(const char *format, ...);
 void logger_end();
+
+#endif /* LOGGER_H */
